b28945f282876e091bfb94d2b853c2d1168298fe
TusharVaid30 <tusharvaid30@gmail.com>
2023-09-27 17:09:39 +0530

    Input

--- Direct3DTest/WinMain.cpp
+++ Direct3DTest/WinMain.cpp
@@ -1,17 +1,57 @@
 #include <Windows.h>
+#include <sstream>
+#include <string>
+
+int a;
+
+LRESULT CALLBACK WndProc(
+	HWND	hWnd, 
+	UINT	msg, 
+	WPARAM	wParam, 
+	LPARAM	lParam
+)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostMessage(hWnd, WM_QUIT, 1, lParam);
+		break;
+	case WM_KEYDOWN:
+		a++;
+		char message[63];
+		sprintf_s(message, "%d\n", a);
+		//SetWindowText(hWnd, message);
+		break;
+
+	case WM_CHAR:
+		{
+			static std::string title;
+			title.push_back((char)wParam);
+			SetWindowText(hWnd, title.c_str());
+		}
+		break;
+
+	default:
+		break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
 
 int CALLBACK WinMain(
-	_In_ HINSTANCE hInstance,
-	_In_opt_ HINSTANCE hPrevInstance,
-	_In_ LPSTR lpCmdLine,
-	_In_ int nCmdShow)
+	_In_	 HINSTANCE	hInstance,
+	_In_opt_ HINSTANCE	hPrevInstance,
+	_In_	 LPSTR		lpCmdLine,
+	_In_	 int		nCmdShow
+)
 {
+	a = 0;
+
 	const auto pClassName = "WINDOW";
 
 	WNDCLASSEX WND = { 0 };
 	WND.cbSize = sizeof(WND);
 	WND.style = CS_OWNDC;
-	WND.lpfnWndProc = DefWindowProc;
+	WND.lpfnWndProc = WndProc;
 	WND.hInstance = hInstance;
 	WND.lpszClassName = pClassName;
 
@@ -25,7 +65,16 @@ int CALLBACK WinMain(
 
 	ShowWindow(hWnd, SW_SHOW);
 
-	while (true);
+	MSG msg;
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg, NULL, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 
-	return 0;
+	if (gResult <= 0)
+		return -1;
+
+	return msg.wParam;
 }
